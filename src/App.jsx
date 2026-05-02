bb596f5be954eb8747fedce168d0827fba194b72
scribencode <deyeye99@gmail.com>
2026-05-02 06:48:22 +0100

    feat: add ingredients section
    - add input tag to get users desired ingredients
    - a button tag to fetch users input and display input
    - add get a recipe section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-
-import './App.css'
-import Header from './components/Header'
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
- 
-
   return (
-    <div className='font-spartan'>
-     <Header/>
-     <main>
-      
-     </main>
+    <div className="font-spartan bg-[#fafaf8] h-lvh">
+      <Header />
+      <main>
+        <Main />
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
